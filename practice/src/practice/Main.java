327e924b6285b7be40befc7e34c473ac8f5df5e0
Muminul Islam <muminul.islam@oracle.com>
2016-12-23 14:22:13 -0800

    matrix

--- practice/src/practice/Main.java
+++ practice/src/practice/Main.java
@@ -3,7 +3,7 @@ package practice;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Matrix.print_2d_matrix(Matrix.get2DMatrix(3, 3), 3, 3);
 
 	}
